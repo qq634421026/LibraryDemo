94620e44bdfb30ecf83aa7ce11a93af5e38646fa
张洪宇 <aroenzhang@sina.com>
2017-06-20 11:10:14 +0800

    add 方法

--- app/src/main/java/com/incoshare/incopat/librarydemo/MainActivity.java
+++ app/src/main/java/com/incoshare/incopat/librarydemo/MainActivity.java
@@ -5,18 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import android.widget.TextView;
 import com.incoshare.incopat.shoplibrary.AActivity;
+import com.incoshare.incopat.shoplibrary.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    TextView text = (TextView) findViewById(R.id.text);
+    text.setText(Utils.getnum() + "");
+  }
 
-    }
-
-    public void goLibrary(View view) {
-        startActivity(new Intent(MainActivity.this, AActivity.class));
-    }
+  public void goLibrary(View view) {
+    startActivity(new Intent(MainActivity.this, AActivity.class));
+  }
 }
---
+++ shoplibrary/src/main/java/com/incoshare/incopat/shoplibrary/Utils.java
@@ -0,0 +1,14 @@
+package com.incoshare.incopat.shoplibrary;
+
+import android.util.Log;
+
+/**
+ * Created by zhanghongyu on 2017/6/20.
+ */
+
+public class Utils {
+  public static int getnum() {
+    Log.i("tag", "=========hello");
+    return 1;
+  }
+}
